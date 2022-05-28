67bcc6cafac4d38193171f620cc06f3f780673b8
Kaisar <kaisarb@gmail.com>
2022-05-28 19:57:55 +0600

    Lombok dependency added

--- src/main/java/com/kb/Springboottutorial2/controller/DepartmentController.java
+++ src/main/java/com/kb/Springboottutorial2/controller/DepartmentController.java
@@ -2,9 +2,12 @@ package com.kb.Springboottutorial2.controller;
 
 import com.kb.Springboottutorial2.entity.Department;
 import com.kb.Springboottutorial2.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,18 +16,24 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger
+            LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department) {
+    public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List <Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
     public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+
         return departmentService.fetchDepartmentById(departmentId);
     }
 
