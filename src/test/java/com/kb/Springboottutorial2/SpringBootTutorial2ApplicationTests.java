f2ce88d04e765d68497a3d39b19cfa0415c8a21a
Kaisar <kaisarb@gmail.com>
2022-05-28 11:16:03 +0600

    Initial commit

---
+++ src/test/java/com/kb/Springboottutorial2/SpringBootTutorial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.Springboottutorial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
